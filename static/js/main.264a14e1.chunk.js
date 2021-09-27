07ddf0352035de7e17a244c2e923a525f28197cd
BoboGamozo <bobogamozo@yandex.by>
2021-09-27 20:30:19 +0300

    Updates

---
+++ static/js/main.264a14e1.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-gh-pages"]=this["webpackJsonpreact-gh-pages"]||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);n(1);var c=n(3),s=n.n(c),i=(n(8),n.p,n(9),n(0));var o=function(e){e&&e instanceof Function&&n.e(3).then(n.bind(null,12)).then((function(t){var n=t.getCLS,c=t.getFID,s=t.getFCP,i=t.getLCP,o=t.getTTFB;n(e),c(e),s(e),i(e),o(e)}))},a=Object(i.jsxs)("h1",{children:["Hello git Pages by ","Peters Pens"]});s.a.render(a,document.getElementById("root")),o()},8:function(e,t,n){},9:function(e,t,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.264a14e1.chunk.js.map
